7716ea111b6d9bac11bd8267204932502026698e
David <David@NATSUIROSHIROIP>
2025-06-12 15:36:10 +0100

    Auto-save: UnitOrderGiver.cs

--- Assets/UnitOrderGiver.cs
+++ Assets/UnitOrderGiver.cs
@@ -1,5 +1,4 @@
-﻿// Assets/UnitOrderGiver.cs
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -10,46 +9,37 @@ public class UnitOrderGiver : MonoBehaviour
 
     void Update()
     {
-        // Right-click issues a move order
+        // Right-click = order move
         if (Input.GetMouseButtonDown(1))
         {
-            Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mouseWorld.z = 0f;
-            GiveMoveOrder(mouseWorld);
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            wp.z = 0f;
+            GiveMoveOrder(wp);
         }
     }
 
     void GiveMoveOrder(Vector3 worldDestination)
     {
-        // 1) Grab all currently selected UnitSelectors
-        var selectors = UnitSelector.GetSelectedUnits(); // List<UnitSelector>
+        // 1) Get selectors & their movers
+        var selectors = UnitSelector.GetSelectedUnits(); // your existing API
+        if (selectors.Count == 0) return;
 
-        // 2) Convert to UnitMover list
         List<UnitMover> movers = new List<UnitMover>(selectors.Count);
-        foreach (var sel in selectors)
-        {
-            var mover = sel.GetComponent<UnitMover>();
-            if (mover != null)
-                movers.Add(mover);
-        }
+        foreach (var s in selectors)
+            if (s.TryGetComponent<UnitMover>(out var m))
+                movers.Add(m);
 
-        if (movers.Count == 0)
-            return; // nothing selected
+        if (movers.Count == 0) return;
 
-        // 3) Compute goal cell on your Tilemap
+        // 2) Build goal cell & flow field
         Vector3Int c3 = Tilemap.WorldToCell(worldDestination);
-        Vector2Int goalCell = new Vector2Int(c3.x, c3.y);
+        Vector2Int goal = new Vector2Int(c3.x, c3.y);
 
-        // 4) Build a single FlowField for that goal
-        int w = Tilemap.size.x;
-        int h = Tilemap.size.y;
-        FlowField field = new FlowField(w, h);
-        field.Generate(goalCell, Tilemap);
+        var field = new FlowField(Tilemap);
+        field.Generate(goal, Tilemap);
 
-        // 5) Hand it to each mover
+        // 3) Hand it off
         foreach (var u in movers)
-        {
-            u.SetFlowField(field, goalCell);
-        }
+            u.SetFlowField(field, goal);
     }
 }
